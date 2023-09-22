42553ee9855ca6dff3c0a83f598d95bee25d9d5e
Manjesh R V <manjeshrv592@gmail.com>
2023-09-22 17:17:48 +0530

    Added main country info section

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,51 @@ const App = () => {
         <Search />
         <Bookmarks />
       </Header>
+      <main className='main'>
+        <div className='main-info'>
+          <div className='country-flag-container'>
+            <img
+              src='https://flagcdn.com/in.svg'
+              alt='India flag'
+              className='country-flag'
+            />
+            {/* <small className='text-primary-400'>
+              The flag of India is composed of three equal horizontal bands of
+              saffron, white and green. A navy blue wheel with twenty-four
+              spokes — the Ashoka Chakra — is centered in the white band.
+            </small> */}
+          </div>
+          <div className='country-primary-info'>
+            <div>
+              <small>Country</small>
+              <h2 className='mb-sm'>India</h2>
+              <small>Region</small>
+              <h4 className='mb-sm'>Asia</h4>
+            </div>
+            <small className='mt-auto'>Neighbours (6)</small>
+            <div className='neighbours'>
+              <span title='Bangladesh'>
+                <img src='https://flagcdn.com/bd.svg' alt='Bangladesh Flag' />
+              </span>
+              <span title='Bhutan'>
+                <img src='https://flagcdn.com/bt.svg' alt='Bhutan Flag' />
+              </span>
+              <span title='Myanmar'>
+                <img src='https://flagcdn.com/mm.svg' alt='Myanmar Flag' />
+              </span>
+              <span title='China'>
+                <img src='https://flagcdn.com/cn.svg' alt='China Flag' />
+              </span>
+              <span title='Nepal'>
+                <img src='https://flagcdn.com/np.svg' alt='Nepal Flag' />
+              </span>
+              <span title='Pakistan'>
+                <img src='https://flagcdn.com/pk.svg' alt='Pakistan Flag' />
+              </span>
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 };
