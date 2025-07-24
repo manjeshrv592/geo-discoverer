eaaae3f4c6807bfecdc969fdb92cc80b657f28eb
manjesh <manjeshrv592@gmail.com>
2025-07-24 11:33:15 +0530

    Test

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const Header = ({ children }) => {
-  return <header className='header'>{children}</header>;
+  return <header className="header">{children}</header>;
 };
 
 export default Header;
